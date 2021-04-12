d54b1b1f9da956fc366ceaa091bbc3a3016dc744
kitsmith29 <33060960+kitsmith29@users.noreply.github.com>
2021-04-12 22:42:24 +0100

    Tidied up class and organised into methods

--- MainWindow.py
+++ MainWindow.py
@@ -17,12 +17,28 @@ class MainWindow(qtw.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-
+        
         self.setWindowTitle("Recipe Manager")
         
-        total_widget = qtw.QWidget()
-        sidebar = Sidebar.SidebarWidget()
-        sidebarContent = sidebar.sidebarContent
+        self.createWidgets()
+        self.configureLayout()
+
+        self.sidebarContent.featured_button.clicked.connect(self.openFeaturedPage)
+        self.sidebarContent.browse_button.clicked.connect(self.openBrowsePage)
+        self.sidebarContent.list_button.clicked.connect(self.openListPage)
+        self.sidebarContent.timetable_button.clicked.connect(self.openTimetablePage)
+        self.sidebarContent.nutrition_button.clicked.connect(self.openNutritionPage)
+        self.sidebarContent.add_button.clicked.connect(self.openAddPage)
+        
+        self.setCentralWidget(self.total_widget)
+
+        self.showMaximized()
+        
+    def createWidgets(self):
+        
+        self.total_widget = qtw.QWidget()
+        self.sidebar = Sidebar.SidebarWidget()
+        self.sidebarContent = sidebar.sidebarContent
         
         self.test_recipe = RecipePage.RecipeWidget()
         self.test_recipe.getRecipe(1)
@@ -33,29 +49,20 @@ class MainWindow(qtw.QMainWindow):
         self.scroll.setWidgetResizable(True)
         self.scroll.setWidget(self.test_recipe)
         self.scroll.setFrameShape(qtw.QFrame.NoFrame)
-
-        sidebarContent.featured_button.clicked.connect(self.openFeaturedPage)
-        sidebarContent.browse_button.clicked.connect(self.openBrowsePage)
-        sidebarContent.list_button.clicked.connect(self.openListPage)
-        sidebarContent.timetable_button.clicked.connect(self.openTimetablePage)
-        sidebarContent.nutrition_button.clicked.connect(self.openNutritionPage)
-        sidebarContent.add_button.clicked.connect(self.openAddPage)
+        
+    def configureLayout(self):
 
         grid_layout = qtw.QGridLayout()
-        grid_layout.addWidget(sidebar, 0, 0)
+        grid_layout.addWidget(self.sidebar, 0, 0)
         grid_layout.addWidget(self.scroll, 0, 1)
         grid_layout.setColumnStretch(1, 15)
         grid_layout.setColumnStretch(0, 1)
         grid_layout.setSpacing(0)
         grid_layout.setContentsMargins(0,0,0,0)
-        total_widget.setLayout(grid_layout)
-
-        self.setCentralWidget(total_widget)
-
-        self.showMaximized()
-            
+        self.total_widget.setLayout(grid_layout) 
         
     def openFeaturedPage(self):
+        
         self.FeaturedPage = FeaturedPage.FeaturedWidget()
 
         self.FeaturedPage.addFeaturedCategory("Vegetarian")
@@ -80,22 +87,27 @@ class MainWindow(qtw.QMainWindow):
         self.scroll.setWidget(self.FeaturedPage)
 
     def openBrowsePage(self):
+        
         self.BrowsePage = BrowsePage.BrowseWidget()
         self.scroll.setWidget(self.BrowsePage)
 
     def openListPage(self):
+        
         self.ListPage = ListPage.ListWidget()
         self.scroll.setWidget(self.ListPage)
 
     def openTimetablePage(self):
+        
         self.TimetablePage = TimetablePage.TimetableWidget()
         self.scroll.setWidget(self.TimetablePage)
 
     def openNutritionPage(self):
+        
         self.NutritionPage = NutritionPage.NutritionWidget()
         self.scroll.setWidget(self.NutritionPage)
 
     def openAddPage(self):
+        
         self.AddPage = AddRecipePage.AddRecipeWidget()
         self.scroll.setWidget(self.AddPage)
         
